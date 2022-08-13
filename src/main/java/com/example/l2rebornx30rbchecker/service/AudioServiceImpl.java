1cd6be01f1964f7860f3a3ebfc833744203bfc8a
Panchev <neno.panchev@gmail.com>
2022-08-13 17:12:22 +0300

    tweaks

--- src/main/java/com/example/l2rebornx30rbchecker/service/AudioServiceImpl.java
+++ src/main/java/com/example/l2rebornx30rbchecker/service/AudioServiceImpl.java
@@ -9,22 +9,22 @@ import java.io.IOException;
 
 @Component
 public class AudioServiceImpl {
-    private final Clip clip = AudioSystem.getClip();
-
-
-    public AudioServiceImpl() throws LineUnavailableException {
-    }
-
-    public void openSound() throws LineUnavailableException, IOException, UnsupportedAudioFileException {
-        File file = new File(GlobalConstants.SOUND_FILE_PATH);
-        AudioInputStream audioIn = AudioSystem.getAudioInputStream(file.toURI().toURL());
-        clip.open(audioIn);
-    }
-
-    public void playSound() {
-        clip.start();
-        clip.setFramePosition(0);
-        clip.flush();
-    }
+//    private final Clip clip = AudioSystem.getClip();
+//
+//
+//    public AudioServiceImpl() throws LineUnavailableException {
+//    }
+//
+//    public void openSound() throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+//        File file = new File(GlobalConstants.SOUND_FILE_PATH);
+//        AudioInputStream audioIn = AudioSystem.getAudioInputStream(file.toURI().toURL());
+//        clip.open(audioIn);
+//    }
+//
+//    public void playSound() {
+//        clip.start();
+//        clip.setFramePosition(0);
+//        clip.flush();
+//    }
 
 }
