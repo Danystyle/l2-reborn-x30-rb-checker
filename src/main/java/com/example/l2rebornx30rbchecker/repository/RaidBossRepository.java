5bd9f488ef4245ffb8702309200597396ae5af78
Panchev <neno.panchev@gmail.com>
2022-08-14 20:44:23 +0300

    added order by isAlive

--- src/main/java/com/example/l2rebornx30rbchecker/repository/RaidBossRepository.java
+++ src/main/java/com/example/l2rebornx30rbchecker/repository/RaidBossRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface RaidBossRepository extends JpaRepository<RaidBoss, Long> {
-    List<RaidBoss> findAllByOrderByAliveRespawnEnd();
+    List<RaidBoss> findAllByOrderByAliveDescRespawnEndAsc();
 }
